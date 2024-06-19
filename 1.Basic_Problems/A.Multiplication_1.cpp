bfe26a50c09d5b75237a8299711b86e18b0e9461
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-06-19 11:53:11 +0600

    Practice of class 1

---
+++ 1.Basic_Problems/A.Multiplication_1.cpp
@@ -0,0 +1,13 @@
+//Problem : https://vjudge.net/problem/AtCoder-abc169_a/origin
+
+//Code: 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,result;
+    cin>>a>>b;
+    result = a*b;
+    cout<<result<<endl;
+    return 0;
+}
