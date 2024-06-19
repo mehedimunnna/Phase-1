baf290ecd2aea4ef7e1d4fe2439a47d94fb4d10e
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-06-19 14:48:29 +0600

    Practice of class 1

---
+++ 1.Basic_Problems/D.Round_decimals.cpp
@@ -0,0 +1,13 @@
+//Problem link: https://atcoder.jp/contests/abc226/tasks/abc226_a?lang=en
+
+//Code: 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double x;
+    cin>>x;
+    cout<<round(x)<<endl;
+    return 0;
+}
