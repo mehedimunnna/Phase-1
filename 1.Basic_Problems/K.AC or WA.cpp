b4fc198d9ccb281b2882afdc1720356ee4c63b7c
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-06-21 15:24:47 +0600

    Practice of class 1

---
+++ 1.Basic_Problems/K.AC or WA.cpp
@@ -0,0 +1,19 @@
+//Problem: https://atcoder.jp/contests/abc152/tasks/abc152_a?lang=en
+
+
+//Code: 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n , m;
+    cin>>n>>m;
+    
+    if(n==m){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
