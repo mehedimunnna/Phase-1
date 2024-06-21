94983a651e832afadefe4a48291e964012e39eeb
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-06-21 15:37:15 +0600

    Practice of class 1

---
+++ 1.Basic_Problems/L.Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+//Problem: https://codeforces.com/problemset/problem/630/A
+//Code: 
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll int n;
+    cin>>n;
+    cout<<25<<endl;
+    return 0;
+}
